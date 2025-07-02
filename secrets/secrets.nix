5e2b9505b3186a6d21d8f71b24a33bfe08c336c1
Joshua Blais <josh@joshblais.com>
2025-07-02 15:01:10 -0600

    secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,6 +2,13 @@ let
   # Your personal SSH public key (from ~/.ssh/joshuakey.pub)
   joshua = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICCWNto66rFbOvb1VDEDuZYdwHQPfKM7+EjpnHvs3eRr joshua@joshuablais.com";
 
+  # Machine SSH host keys
+  # king = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBb root@king";
+  # theologica = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCc root@theologica";
+  # alexandria = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDd root@alexandria";
+  # empire = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEe root@empire";
+
+
   # Groups for convenience
   users = [ joshua ];
   # desktops = [ king theologica ];
