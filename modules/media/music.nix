161126865cedc05ccc41d06774fdd5fe95c3b96f
Joshua Blais <josh@joshblais.com>
2025-07-03 14:15:17 -0600

    music

--- modules/media/music.nix
+++ modules/media/music.nix
@@ -1,1 +1,18 @@
+{ config, pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    # Music players
+    mpd
+    ncmpcpp
+    spotify
 
+    # Audio tools
+    audacity
+    lmms
+
+    # Audio codecs
+    ffmpeg
+    flac
+    lame
+  ];
+}
