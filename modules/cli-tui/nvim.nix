3431d243069e1072d3ca7d499e66b62cdcedd8aa
Joshua Blais <josh@joshblais.com>
2025-07-03 13:59:47 -0600

    nvim added

---
+++ modules/cli-tui/nvim.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+  # System-level tmux installation
+  environment.systemPackages = with pkgs; [
+    nvim
+  ];
+
+  # Home-manager will handle the config via symlink
+  programs.nvim.enable = true;
+}
