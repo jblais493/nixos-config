33cbcf9c26f8fe44029e9482909dae361e13570e
Joshua Blais <josh@joshblais.com>
2025-07-03 14:10:07 -0600

    media and dev defaults

---
+++ modules/development/default.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./git.nix
+    ./zsh.nix
+    # Add ./nvim.nix and ./tmux.nix when you create them
+  ];
+
+  # CLI/TUI tools
+  environment.systemPackages = with pkgs; [
+    # File management
+    eza              # Better ls
+    yazi             # Terminal file manager
+    bat              # Better cat
+    fd               # Better find
+    ripgrep-all      # Better grep
+    fzf              # Fuzzy finder
+
+    # Git tools
+    lazygit          # Git TUI
+
+    # System tools
+    btop             # Better top
+    trash-cli        # Safe rm
+    tldr             # Simplified man pages
+    fastfetch        # System info
+  ];
+}
---
+++ modules/media/default.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./music.nix
+    ./video.nix
+    ./pdf.nix
+  ];
+
+  # Common media packages
+  environment.systemPackages = with pkgs; [
+    # Image viewers
+    feh
+    imv
+
+    # Media players
+    mpv
+    vlc
+
+    # Audio tools
+    pavucontrol
+    alsa-utils
+
+    # Document viewers
+    zathura
+    evince
+  ];
+}
