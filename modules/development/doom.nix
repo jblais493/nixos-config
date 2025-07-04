5ee2440617a03a8a01e3004dc9a3fdc346c5018e
Joshua Blais <josh@joshblais.com>
2025-07-03 20:46:24 -0600

    doom.nix

--- modules/development/doom.nix
+++ modules/development/doom.nix
@@ -1,14 +1,18 @@
 { config, pkgs, ... }:
-
 {
-  # Install Emacs
-  home.packages = with pkgs; [
+  # Install Emacs and Doom dependencies at system level
+  environment.systemPackages = with pkgs; [
     emacs
     # Doom Emacs dependencies
     ripgrep
     fd
     imagemagick
-    texliveFull  # For LaTeX support
+    texlive.combined.scheme-full  # For LaTeX support
     sqlite       # For org-roam
+
+    # Additional tools Doom might need
+    git
+    curl
+    wget
   ];
 }
