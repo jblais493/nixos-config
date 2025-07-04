de23d69b014ecd0b4e37709520b34deb5c9029e3
Joshua Blais <josh@joshblais.com>
2025-07-04 14:45:33 -0600

    nord

--- modules/home-manager/browsers.nix
+++ modules/home-manager/browsers.nix
@@ -17,6 +17,8 @@
         violentmonkey
         wayback-machine
         leechblock-ng
+        nord-theme
+        sideberry
       ];
 
       settings = {
@@ -41,6 +43,39 @@
         "browser.cache.disk.enable" = false;
         "browser.sessionstore.privacy_level" = 2;
       };
+
+      # Custom userChrome.css for vertical tabs
+      userChrome = ''
+        /* Hide horizontal tabs when using Tree Style Tab */
+        #TabsToolbar {
+          visibility: collapse !important;
+        }
+
+        /* Nord color scheme for Firefox UI */
+        :root {
+          --nord0: #2e3440;
+          --nord1: #3b4252;
+          --nord2: #434c5e;
+          --nord3: #4c566a;
+          --nord4: #d8dee9;
+          --nord5: #e5e9f0;
+          --nord6: #eceff4;
+          --nord7: #8fbcbb;
+          --nord8: #88c0d0;
+          --nord9: #81a1c1;
+          --nord10: #5e81ac;
+        }
+
+        /* Apply Nord colors to browser chrome */
+        #navigator-toolbox {
+          background-color: var(--nord1) !important;
+        }
+
+        #urlbar {
+          background-color: var(--nord2) !important;
+          color: var(--nord6) !important;
+        }
+      '';
 
       # Updated bookmarks syntax
       bookmarks = {
