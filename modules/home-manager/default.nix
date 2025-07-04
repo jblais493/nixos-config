3ab71bd697d04032538806d8927267eb5c99e37d
Joshua Blais <josh@joshblais.com>
2025-07-03 21:18:44 -0600

    try homemanager

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,5 +1,9 @@
 { config, lib, pkgs, inputs, ... }:
 
+let
+  # Create a function to make out-of-store symlinks
+  mkOutOfStoreSymlink = path: config.lib.file.mkOutOfStoreSymlink path;
+in
 {
   imports = [
     ./git.nix
@@ -14,16 +18,16 @@
   # Let home-manager manage itself
   programs.home-manager.enable = true;
 
-  # Use relative paths from the flake root
+  # Use mkOutOfStoreSymlink for live editing
   home.file = {
-    ".config/doom".source = ../../dotfiles/doom;
-    ".config/tmux".source = ../../dotfiles/tmux;
-    ".config/hypr".source = ../../dotfiles/hypr;
-    ".config/waybar".source = ../../dotfiles/waybar;
-    ".config/swaync".source = ../../dotfiles/swaync;
-    ".config/wofi".source = ../../dotfiles/wofi;
-    ".config/nvim".source = ../../dotfiles/nvim;
-    ".config/zathura".source = ../../dotfiles/zathura;
-    ".tmux.conf".source = ../../dotfiles/tmux/.tmux.conf;
+    ".config/doom".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/dotfiles/doom";
+    ".config/tmux".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/dotfiles/tmux";
+    ".config/hypr".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/dotfiles/hypr";
+    ".config/waybar".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/dotfiles/waybar";
+    ".config/swaync".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/dotfiles/swaync";
+    ".config/wofi".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/dotfiles/wofi";
+    ".config/nvim".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/dotfiles/nvim";
+    ".config/zathura".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/dotfiles/zathura";
+    ".tmux.conf".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/dotfiles/tmux/.tmux.conf";
   };
 }
