7d0fb216b5bb9dfa788731d7b6fd32b164eed2e3
Joshua Blais <josh@joshblais.com>
2025-08-08 09:55:45 -0600

    adding in astro export .md

--- dotfiles/doom/lisp/post-to-blog.el
+++ dotfiles/doom/lisp/post-to-blog.el
@@ -1,37 +1,60 @@
 ;;; ../../dotfiles/doom/.config/doom/lisp/post-to-blog.el -*- lexical-binding: t; -*-
 
-(defun publish-post-to-hugo ()
-  "Select and publish an org file from the writing directory to Hugo blog."
+(defun my/get-org-property (property)
+  "Get an org property from the current buffer."
+  (save-excursion
+    (goto-char (point-min))
+    (when (re-search-forward (format "^#\\+%s: \\(.*\\)$" property) nil t)
+      (match-string 1))))
+
+(defun my/get-org-filetags ()
+  "Get filetags from current buffer as a list."
+  (let ((tags-string (my/get-org-property "FILETAGS")))
+    (when tags-string
+      (split-string tags-string))))
+
+(defun my/generate-astro-frontmatter ()
+  "Generate Astro frontmatter from current org buffer."
+  (let* ((title (or (my/get-org-property "TITLE")
+                    (file-name-base (buffer-file-name))))
+         (date (or (my/get-org-property "DATE")
+                   (format-time-string "%Y-%m-%d")))
+         (lastmod (or (my/get-org-property "LASTMOD") ""))
+         (toc (or (my/get-org-property "TOC") "true"))
+         (share (or (my/get-org-property "SHARE") "true"))
+         (giscus (or (my/get-org-property "GISCUS") "true"))
+         (ogimage (or (my/get-org-property "OGIMAGE") "true"))
+         (tags (my/get-org-filetags)))
+    (format "---\ntitle: %s\npubDate: %s\nlastModDate: '%s'\ntoc: %s\nshare: %s\ngiscus: %s\nogImage: %s\ntags: [%s]\n---\n\n"
+            title date lastmod toc share giscus ogimage
+            (if tags
+                (mapconcat (lambda (tag) (format "\"%s\"" tag)) tags ", ")
+              ""))))
+
+(defun publish-post-to-astro ()
+  "Select and publish an org file from the writing directory to Astro blog."
   (interactive)
   (let* ((writing-dir (expand-file-name "~/org/roam/writing/To Post/"))
-         (hugo-posts-dir (expand-file-name "~/Development/Blog2024/content/posts/"))
-         ;; Get all org files in the writing directory
+         (astro-posts-dir (expand-file-name "~/Development/joshuablais.com/src/content/blog/"))
          (org-files (directory-files writing-dir t "\\.org$"))
-         ;; Create a list of just the file names for selection
          (file-names (mapcar #'file-name-nondirectory org-files))
-         ;; Have the user select a file
          (selected-name (completing-read "Select post to publish: " file-names nil t))
-         ;; Find the full path of the selected file
          (selected-file (car (seq-filter
                               (lambda (f) (string= (file-name-nondirectory f) selected-name))
                               org-files)))
-         ;; Prepare the destination path
          (file-base-name (file-name-sans-extension selected-name))
          (kebab-name (replace-regexp-in-string " " "-" (downcase file-base-name)))
-         (post-dir (concat hugo-posts-dir kebab-name "/"))
-         (target-file (concat post-dir "index.org")))
-
-    ;; Confirm with the user
-    (when (yes-or-no-p (format "Publish %s to %s?" selected-name post-dir))
-      ;; Create the post directory if it doesn't exist
-      (unless (file-exists-p post-dir)
-        (make-directory post-dir t))
-
-      ;; Copy the file to the target location
-      (copy-file selected-file target-file t)
-
+         (target-file (concat astro-posts-dir kebab-name ".md")))
+    (when (yes-or-no-p (format "Publish %s to %s?" selected-name target-file))
+      (with-current-buffer (find-file-noselect selected-file)
+        ;; Generate frontmatter from current buffer
+        (let ((frontmatter (my/generate-astro-frontmatter))
+              ;; Export to markdown without frontmatter
+              (content (org-export-as 'md nil nil t)))
+          ;; Write combined result
+          (with-temp-file target-file
+            (insert frontmatter)
+            (insert content))))
       (message "Published %s to %s" selected-name target-file)
-
-      ;; Optionally open the target file
-      (when (yes-or-no-p "Open the published file?")
+      (when (yes-or-no-p "Open the published markdown file?")
         (find-file target-file)))))
