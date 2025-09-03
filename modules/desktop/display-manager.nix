4d9ff0dbaa7e136b97a5576df1222930112d08a6
Joshua Blais <josh@joshblais.com>
2025-09-02 18:38:28 -0600

    display manager

--- modules/desktop/display-manager.nix
+++ modules/desktop/display-manager.nix
@@ -1,12 +1,10 @@
 { config, pkgs, ... }:
 {
-  # Copy your profile image to the system store
   environment.etc."profiles/joshua.png" = {
-    source = /home/joshua/Pictures/joshua.png;
+    source = ./joshua.png;  # Relative to your config file
     mode = "0644";
   };
 
-  # Configure AccountsService for your user
   environment.etc."AccountsService/users/joshua" = {
     text = ''
       [User]
@@ -16,6 +14,5 @@
     mode = "0644";
   };
 
-  # Ensure AccountsService is enabled (usually automatic with GDM)
   services.accounts-daemon.enable = true;
 }
