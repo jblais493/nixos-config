5ec3865d8882dfb5233aaaa7b6f674baa000f93e
Joshua Blais <josh@joshblais.com>
2025-09-02 18:43:34 -0600

    update display manager

--- modules/desktop/display-manager.nix
+++ modules/desktop/display-manager.nix
@@ -1,18 +1,10 @@
 { config, pkgs, ... }:
 {
-  environment.etc."profiles/joshua.png" = {
-    source = ./joshua.png;  # Relative to your config file
-    mode = "0644";
-  };
+  # Enable GDM display manager (updated syntax)
+  services.xserver.enable = true;
 
-  environment.etc."AccountsService/users/joshua" = {
-    text = ''
-      [User]
-      SystemAccount=false
-      Icon=/etc/profiles/joshua.png
-    '';
-    mode = "0644";
+  services.displayManager.gdm = {
+    enable = true;
+    wayland = true;
   };
-
-  services.accounts-daemon.enable = true;
 }
