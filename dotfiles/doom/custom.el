376be9d4d448e7287affb8903114f29889b53677
Joshua Blais <josh@joshblais.com>
2025-10-08 10:19:47 -0600

    add files for tracking

--- dotfiles/doom/custom.el
+++ dotfiles/doom/custom.el
@@ -11,8 +11,9 @@
      "7ea883b13485f175d3075c72fceab701b5bf76b2076f024da50dff4107d0db25" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
-   '("~/Development/go-backup/README.org" "/home/joshua/org/caldav-inbox.org"
-     "/home/joshua/org/done.org"
+   '("/home/joshua/Development/edifice/README.org"
+     "/home/joshua/Development/go-backup/README.org"
+     "/home/joshua/org/caldav-inbox.org" "/home/joshua/org/done.org"
      "/home/joshua/Development/luminaforge/README.org"
      "/home/joshua/Projects/MaisonOS/README.org"
      "/home/joshua/nixos-config/dotfiles/doom/README.org"
