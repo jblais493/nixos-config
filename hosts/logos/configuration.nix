19352997ce99a37b3dbe7349c79cedab5389efc4
Joshua Blais <josh@joshblais.com>
2025-12-04 11:56:19 -0700

    logos

---
+++ hosts/logos/configuration.nix
@@ -0,0 +1,44 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../profiles/bishop.nix
+    ../../modules/secrets-joshua.nix
+  ];
+
+  # Host-specific configuration
+  networking.hostName = "logos";
+
+  # Boot loader configuration
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Define your user properly
+  users.users.joshua = {
+    isNormalUser = true;
+    description = "Joshua Blais";
+    group = "joshua";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+  };
+
+  # Create the user group
+  users.groups.joshua = { };
+
+  # Basic system configuration
+  time.timeZone = "America/Edmonton";
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  # Enable Supernote sync tool
+  services.supernote-watcher.enable = true;
+
+  # Set the state version
+  system.stateVersion = "25.11";
+}
