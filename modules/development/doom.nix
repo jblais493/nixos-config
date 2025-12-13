899e98af9539f18f8e0b4384af1f3f863f99f47c
Joshua Blais <josh@joshblais.com>
2025-12-13 12:40:35 -0700

    fix emacs home manager/system
    emacs is now managed at home-manager level
    
    type -a emacs
    emacs is an alias for GDK_BACKEND=x11 emacs
    emacs is /etc/profiles/per-user/joshua/bin/emacs

--- modules/development/doom.nix
+++ modules/development/doom.nix
@@ -1,45 +1,28 @@
 { config, pkgs, ... }:
 {
-  # Install Emacs and Doom dependencies at system level
   environment.systemPackages = with pkgs; [
-    emacs
-    emacsPackages.mu4e
-    # Doom Emacs dependencies
     ripgrep
     vips
     wmctrl
     fd
     imagemagick
-    texlive.combined.scheme-full # For LaTeX support
-    sqlite # For org-roam
+    texlive.combined.scheme-full
+    sqlite
     aspell
     aspellDicts.en
-    aspellDicts.en-computers # technical terms
-    aspellDicts.en-science # scientific terms
-
-    # Additional tools Doom might need
+    aspellDicts.en-computers
+    aspellDicts.en-science
     git
     curl
     wget
-
-    # Additional Doom dependencies
-    nodejs # For LSP servers
+    nodejs
     nodePackages.js-beautify
-    shellcheck # For shell script checking
-    html-tidy # For HTML formatting
-    stylelint # For CSS linting
+    shellcheck
+    html-tidy
+    stylelint
     ledger
     shfmt
     graphviz
-
-    # Fonts that Doom recommends
     emacs-all-the-icons-fonts
   ];
-
-  # System-level Emacs configuration
-  services.emacs = {
-    enable = true;
-    package = pkgs.emacs;
-    defaultEditor = true; # Set Emacs as default editor
-  };
 }
--- modules/home-manager/emacs.nix
+++ modules/home-manager/emacs.nix
@@ -2,7 +2,7 @@
 {
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs;
+    package = pkgs.emacs; # This is the ONLY emacs installation
     extraPackages =
       epkgs: with epkgs; [
         doom
@@ -10,7 +10,31 @@
         pdf-tools
         org-roam
         treesit-grammars.with-all-grammars
+        mu4e # Move this from system level
       ];
+  };
+
+  # Add the systemd user service
+  systemd.user.services.emacs = {
+    Unit = {
+      Description = "Emacs daemon";
+      After = [ "graphical-session-pre.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      Type = "notify";
+      ExecStart = "${config.programs.emacs.finalPackage}/bin/emacs --fg-daemon";
+      ExecStop = "${config.programs.emacs.finalPackage}/bin/emacsclient --eval '(kill-emacs)'";
+      Restart = "on-failure";
+
+      Environment = [
+        "PATH=${config.home.profileDirectory}/bin:/run/current-system/sw/bin"
+        "GDK_BACKEND=x11"
+      ];
+    };
+
+    Install.WantedBy = [ "graphical-session.target" ];
   };
 
   home.sessionPath = [ "${config.home.homeDirectory}/.emacs.d/bin" ];
